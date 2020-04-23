e222e19eafc7803c3b2551f7d910b135d81e0eda
Bogdan-wq <58434080+Bogdan-wq@users.noreply.github.com>
2020-04-23 17:09:35 +0300

    'second-crowdme'

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,9 +1,18 @@
 const tabs = document.getElementById('tabs-containerJS').getElementsByClassName('tabs__block');
+const nums = document.getElementsByClassName('toggler-item__num');
 
 
-function toggle(id) {
+function toggle(id,elmnt) {
     for(let i = 0 ; i < tabs.length; i++) {
         tabs[i].classList.remove('tabs__block_active');
+
     }
+
+    for(let i = 0; i < nums.length; i++) {
+        nums[i].style.backgroundColor = "#5f5f5f";
+    }
+
+    elmnt.getElementsByClassName('toggler-item__num')[0].style.backgroundColor = "#bcda5c";
+
     document.getElementById(id).classList.add('tabs__block_active');
 }
